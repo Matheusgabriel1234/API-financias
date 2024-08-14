cf0abde3088ab45bf26f0f601bd9b4457bb568e1
Matheusgabriel1234 <magaje2005@gmail.com>
2024-08-14 15:26:25 -0300

    First commit

---
+++ src/main/java/desafio/matheus/desafio_tabela_financias/DesafioTabelaFinanciasApplication.java
@@ -0,0 +1,13 @@
+package desafio.matheus.desafio_tabela_financias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTabelaFinanciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTabelaFinanciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafio/matheus/desafio_tabela_financias/DesafioTabelaFinanciasApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.matheus.desafio_tabela_financias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTabelaFinanciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
